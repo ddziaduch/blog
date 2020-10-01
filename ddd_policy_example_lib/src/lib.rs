857dba3ee9ed55e4fd157cc0f7412ac957032d05
Damian Dziaduch <damian@dziaduch.pl>
2020-10-01 22:11:35 +0200

    DDD Policy Example
    Rust example fixed!

--- ddd_policy_example_lib/src/lib.rs
+++ ddd_policy_example_lib/src/lib.rs
@@ -1,32 +1,45 @@
-struct Bill<'a> {
-    total: &'a f64
+struct Bill {
+    total: f64
 }
 
 trait DiscountPolicy {
-    fn apply(bill: &mut Bill);
+    fn apply(bill: &Bill) -> Bill;
 }
 
 struct ApplicationUserDiscountPolicy {}
 
 impl DiscountPolicy for ApplicationUserDiscountPolicy {
-    fn apply(bill: &mut Bill) {
-        bill.total = &(bill.total * 0.9);
+    fn apply(bill: &Bill) -> Bill {
+        Bill {
+            total: bill.total * 0.9
+        }
     }
 }
 
 struct PensionerDiscountPolicy {}
 
 impl DiscountPolicy for PensionerDiscountPolicy {
-    fn apply(bill: &mut Bill) {
-        bill.total = &(bill.total * 0.85);
+    fn apply(bill: &Bill) -> Bill {
+        Bill {
+            total: bill.total * 0.85
+        }
     }
 }
 
 #[test]
 fn application_user_discount_policy_should_reduce_the_bill_total_by_ten_percents() {
-    let mut bill = Bill {
-        total: &100.0
+    let initial_bill = Bill {
+        total: 100.0
     };
-    ApplicationUserDiscountPolicy::apply(&mut bill);
-    assert_eq!(bill.total, &(90.0 as f64));
+    let discounted_bill = ApplicationUserDiscountPolicy::apply(&initial_bill);
+    assert_eq!(discounted_bill.total, 90.0);
+}
+
+#[test]
+fn pensioner_discount_policy_should_reduce_the_bill_total_by_fifteen_percents() {
+    let initial_bill = Bill {
+        total: 100.0
+    };
+    let discounted_bill = PensionerDiscountPolicy::apply(&initial_bill);
+    assert_eq!(discounted_bill.total, 85.0);
 }
